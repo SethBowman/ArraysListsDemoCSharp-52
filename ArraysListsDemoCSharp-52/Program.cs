a4b13bb095fe040491d1e58c58669db52543c794
Seth Bowman <brandonsethbowman@gmail.com>
2024-12-05 15:35:18 -0600

    demo completed

---
+++ ArraysListsDemoCSharp-52/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace ArraysListsDemoCSharp_52;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //arrays
+        //arrays have a set size that you define at creation and that size cannot change
+
+        int[] numbers = new int[5];
+
+        numbers[4] = 5000;
+        numbers[0] = 25;
+
+        foreach (var number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+        
+        var names = new string[] { "John", "Jane", "Doe" };
+        
+        //arrays have a property called 'Length' that shows you the number of items in the array
+        Console.WriteLine(names.Length);
+        Console.WriteLine(names.Length - 1);
+
+        for (int i = 0; i <= names.Length - 1; i++)
+        {
+            Console.WriteLine(names[i]);
+        }
+        
+        //lists
+        //lists have a size that can grow and shrink all you need
+        List<int> numbersList = new List<int>();
+        
+        numbersList.Add(25);
+        numbersList.Add(12);
+        numbersList.Add(15);
+        
+        //lists have a property called 'Count' that shows you the numbers of elements in the list
+
+        Console.WriteLine(numbersList.Count - 1);
+
+        for (int i = 0; i <= numbersList.Count - 1; i++)
+        {
+            Console.WriteLine(numbersList[i]);
+        }
+       
+    }
+}
